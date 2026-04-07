eb4e4fc8a713090643db7520c3c0443fa0688608
MawadaG <mawadaghalab@gmail.com>
2026-04-08 00:20:05 +0300

    fix: resolve Render deployment issues with frontend rendering and API integration

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "https://smart-task-manager-g7mar.onrender.com/";
 
 const taskList = document.getElementById("taskList");
 const taskMessage = document.getElementById("taskMessage");
